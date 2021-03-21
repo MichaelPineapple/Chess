7206f8a22d94454dedfeae73c6597eb6bcd95328
Mcl <michael707wilson@gmail.com>
2021-03-20 20:27:58 -0400

    Initial Commit

---
+++ chess/Engine.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace chess
+{
+    public static class Toolbox
+    {
+        public static void rIMAGE(TysonBitmap image, double x, double y, double w, double h)
+        {
+            image.Render(x, y, w, h, Color.White);
+        }
+
+        public const double TwoPi = Math.PI * 2, piOverTwo = Math.PI / 2;
+        public static void rCIRCLE(double x, double y, double radius, int triangleAmount, Color hue)
+        {
+            List<Vector2d> vertz = new List<Vector2d>();
+            for (int i = 0; i <= triangleAmount; i++)
+            {
+                vertz.Add(new Vector2d(
+                    x + (radius * Math.Cos(i * TwoPi / triangleAmount)),
+                    y + (radius * Math.Sin(i * TwoPi / triangleAmount))));
+            }
+            vertz.Add(new Vector2d(x, y));
+            rSHAPE(hue, PrimitiveType.TriangleFan, vertz.ToArray());
+        }
+        public static void rCIRCLE(double x, double y, double radius, Color hue)
+        {
+            rCIRCLE(x, y, radius, 20, hue);
+        }
+        public static void rCIRCLE(Vector2d p, double radius, Color hue)
+        {
+            rCIRCLE(p.X, p.Y, radius, hue);
+        }
+
+        public static void rBOX(double _x, double _y, double _size, Color _c)
+        {
+            rBOX(_x, _y, _size, _size, _c);
+        }
+        public static void rBOX(Vector2d p, double _size, Color _c)
+        {
+            rBOX(p.X, p.Y, _size, _size, _c);
+        }
+        public static void rBOX(Vector2d p, double _w, double _h, Color _c)
+        {
+            rBOX(p.X, p.Y, _w, _h, _c);
+        }
+        public static void rBOX(double _x, double _y, double _w, double _h, Color _c)
+        {
+            rBOX(_x, _y, _w, _h, false, _c);
+        }
+        public static void rBOX(double _x, double _y, double _w, double _h, bool centered, Color _c)
+        {
+            Vector2d[] vectList = new Vector2d[4];
+            if (centered)
+            {
+                double w = _w / 2, h = _h / 2;
+                vectList[0] = new Vector2d(_x - w, _y + h);
+                vectList[1] = new Vector2d(_x - w, _y - h);
+                vectList[2] = new Vector2d(_x + w, _y - h);
+                vectList[3] = new Vector2d(_x + w, _y + h);
+            }
+            else
+            {
+                vectList[0] = new Vector2d(_x, _y);
+                vectList[1] = new Vector2d(_x + _w, _y);
+                vectList[2] = new Vector2d(_x + _w, _y + _h);
+                vectList[3] = new Vector2d(_x, _y + _h);
+            }
+            rSHAPE(_c, PrimitiveType.Quads, vectList);
+        }
+        public static void rSHAPE(Color _c, PrimitiveType _pt, params Vector2d[] _v)
+        {
+            GL.Enable(EnableCap.Blend);
+            GL.Begin(_pt);
+            GL.Color4(_c);
+            for (int i = 0; i < _v.Length; i++) V(_v[i]);
+            GL.End();
+            GL.Disable(EnableCap.Blend);
+        }
+        private static void V(double _x, double _y)
+        {
+            GL.Vertex2(_x, _y);
+        }
+        private static void V(Vector2d _p)
+        {
+            V(_p.X, _p.Y);
+        }
+    }
+
+    public class TysonBitmap
+    {
+        int image_id = 0;
+
+        public TysonBitmap(string ImageDirectory)
+        {
+            Bitmap image = new Bitmap(ImageDirectory);
+            GL.GenTextures(1, out image_id);
+            BitmapData bitmapdata;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            bitmapdata = image.LockBits(rect,
+                System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.BindTexture(TextureTarget.Texture2D, image_id);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Nearest);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0,
+                (OpenTK.Graphics.OpenGL.PixelFormat)(int)All.Bgra, PixelType.UnsignedByte, bitmapdata.Scan0);
+        }
+        public TysonBitmap(Bitmap image)
+        {
+            GL.GenTextures(1, out image_id);
+            BitmapData bitmapdata;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            bitmapdata = image.LockBits(rect,
+                System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.BindTexture(TextureTarget.Texture2D, image_id);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Nearest);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0,
+                (OpenTK.Graphics.OpenGL.PixelFormat)(int)All.Bgra, PixelType.UnsignedByte, bitmapdata.Scan0);
+        }
+
+
+        public void Render(double x, double y, double w, double h, Color c)
+        {
+            GL.Enable(EnableCap.Texture2D);
+            GL.BindTexture(TextureTarget.Texture2D, this.image_id);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(c);
+            GL.TexCoord2(0, 1); GL.Vertex2(x - w, y + h);
+            GL.TexCoord2(1, 1); GL.Vertex2(x + w, y + h);
+            GL.TexCoord2(1, 0); GL.Vertex2(x + w, y - h);
+            GL.TexCoord2(0, 0); GL.Vertex2(x - w, y - h);
+            GL.End();
+            GL.Disable(EnableCap.Texture2D);
+            GL.Flush();
+        }
+
+        public int getID()
+        {
+            return this.image_id;
+        }
+    }
+
+}
+
---
+++ chess/Program.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK.Input;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace chess
+{
+    class Program : GameWindow
+    {
+        static void Main()
+        {
+            Program E;
+
+            try
+            {
+                E = new Program(800, 800, "Wislon Chess");
+                E.Run(60);
+            }
+            catch
+            {
+                MessageBox.Show("ERROR!");
+            }
+        }
+
+        const int E = 0, P = 1, R = 2, B = 3, H = 4, K = 5, Q = 6, 
+            SQRNUM = 64, SQRSIZE = 100, RN = 8, HALFSQRSIZE = SQRSIZE / 2, RNSUBONE = RN - 1;
+        int mouseHoverIndex = -1, selectIndex = -1;
+        Vector2d[] chessBoardPositions = new Vector2d[SQRNUM];
+        bool bw = true, turn = false;
+        Color ColorA = Color.White, ColorB = Color.Black, ColorC = Color.Violet;
+        List<int> moveList = new List<int>();
+        TysonBitmap whitePawn, whiteRook, whiteBishop, whiteKnight, whiteKing, whiteQueen,
+            blackPawn, blackRook, blackBishop, blackKnight, blackKing, blackQueen;
+
+        int[] chessBoard =
+        {
+            +R, +H, +B, +Q, +K, +B, +H, +R,
+            +P, +P, +P, +P, +P, +P, +P, +P,
+            +E, +E, +E, +E, +E, +E, +E, +E,
+            +E, +E, +E, +E, +E, +E, +E, +E,
+            -E, -E, -E, -E, -E, -E, -E, -E,
+            -E, -E, -E, -E, -E, -E, -E, -E,
+            -P, -P, -P, -P, -P, -P, -P, -P,
+            -R, -H, -B, -K, -Q, -B, -H, -R,
+        };
+
+        Program(int _windowWidth, int _windowHeight, string _windowName) : base(_windowWidth, _windowHeight, GraphicsMode.Default, _windowName)
+        {
+            // LOAD ASSETS
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            Icon = new Icon(directory + @"\icon.ico");
+            whitePawn = new TysonBitmap(directory + @"\whitePawn.png");
+            whiteRook = new TysonBitmap(directory + @"\whiteRook.png");
+            whiteBishop = new TysonBitmap(directory + @"\whiteBishop.png");
+            whiteKnight = new TysonBitmap(directory + @"\whiteKnight.png");
+            whiteKing = new TysonBitmap(directory + @"\whiteKing.png");
+            whiteQueen = new TysonBitmap(directory + @"\whiteQueen.png");
+            blackPawn = new TysonBitmap(directory + @"\blackPawn.png");
+            blackRook = new TysonBitmap(directory + @"\blackRook.png");
+            blackBishop = new TysonBitmap(directory + @"\blackBishop.png");
+            blackKnight = new TysonBitmap(directory + @"\blackKnight.png");
+            blackKing = new TysonBitmap(directory + @"\blackKing.png");
+            blackQueen = new TysonBitmap(directory + @"\blackQueen.png");
+
+            // SETUP
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Viewport(ClientRectangle);
+            GL.LoadIdentity();
+            GL.Ortho(0, Width, Height, 0, -1, 0);
+            for (int i = 0; i < SQRNUM; i++) chessBoardPositions[i] = new Vector2d(HALFSQRSIZE + ((i % RN) * SQRSIZE), HALFSQRSIZE + ((i / RN) * SQRSIZE));
+            swapTurn();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            mouseHoverIndex = getHover(new Vector2d(Mouse.X, Mouse.Y));
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.ClearColor(Color.Black);
+
+            moveList.Clear();
+
+            // render board
+            for (int i = 0; i < SQRNUM; i++)
+            {
+                Color squareColor = ColorB;
+                if (bw) squareColor = ColorA;
+                renderSquare(chessBoardPositions[i], squareColor);
+                if (i % RN != RNSUBONE) bw = !bw;
+            }
+
+            // render pieces
+            for (int i = 0; i < SQRNUM; i++)
+            {
+                Vector2d pos = chessBoardPositions[i];
+
+                switch (chessBoard[i])
+                {
+                    case P:
+                        renderImage(pos, whitePawn);
+                        break;
+                    case R:
+                        renderImage(pos, whiteRook);
+                        break;
+                    case B:
+                        renderImage(pos, whiteBishop);
+                        break;
+                    case H:
+                        renderImage(pos, whiteKnight);
+                        break;
+                    case K:
+                        renderImage(pos, whiteKing);
+                        break;
+                    case Q:
+                        renderImage(pos, whiteQueen);
+                        break;
+                    case -P:
+                        renderImage(pos, blackPawn);
+                        break;
+                    case -R:
+                        renderImage(pos, blackRook);
+                        break;
+                    case -B:
+                        renderImage(pos, blackBishop);
+                        break;
+                    case -H:
+                        renderImage(pos, blackKnight);
+                        break;
+                    case -K:
+                        renderImage(pos, blackKing);
+                        break;
+                    case -Q:
+                        renderImage(pos, blackQueen);
+                        break;
+                }
+            }
+
+
+            // select
+            if (selectIndex != -1)
+            {
+                int y = getY(selectIndex), x = getX(selectIndex);
+
+                switch (chessBoard[selectIndex])
+                {
+                    // pawn
+                    case P:
+                        int q = selectIndex + RN + 1;
+                        if (!isSpaceEmpty(q) && x < 7) addMove(q);
+                        q = selectIndex + RN - 1;
+                        if (!isSpaceEmpty(q) && x > 0) addMove(q);
+                        q = selectIndex + RN;
+                        if (!isSpaceEmpty(q)) break;
+                        addMove(q);
+                        if (y == 1) addMove(selectIndex + RN + RN);
+                        break;
+                    case -P:
+                        q = selectIndex - RN + 1;
+                        if (!isSpaceEmpty(q) && x < 7) addMove(q);
+                        q = selectIndex - RN - 1;
+                        if (!isSpaceEmpty(q) && x > 0) addMove(q);
+                        q = selectIndex - RN;
+                        if (!isSpaceEmpty(q)) break;
+                        addMove(q);
+                        if (y == 6) addMove(selectIndex - RN - RN);
+                        break;
+
+                    // rook
+                    case R:
+                    case -R:
+                        addRookMoves(selectIndex);
+                        break;
+
+                    // bishop
+                    case B:
+                    case -B:
+                        addBishopMoves(selectIndex);
+                        break;
+
+                    // knight
+                    case H:
+                    case -H:
+                        if (x > 0)
+                        {
+                            addMove(selectIndex - 17);
+                            addMove(selectIndex + 15);
+                        }
+                        if (x > 1)
+                        {
+                            addMove(selectIndex - 10);
+                            addMove(selectIndex + 6);
+                        }
+                        if (x < 6)
+                        {
+                            addMove(selectIndex + 10);
+                            addMove(selectIndex - 6);
+                        }
+                        if (x < 7)
+                        {
+                            addMove(selectIndex + 17);
+                            addMove(selectIndex - 15);
+                        }
+                        break;
+
+                    // queen
+                    case Q:
+                    case -Q:
+                        addBishopMoves(selectIndex);
+                        addRookMoves(selectIndex);
+                        break;
+
+                    // king
+                    case K:
+                    case -K:
+                        addMove(selectIndex + RN);
+                        addMove(selectIndex - RN);
+                        addMove(selectIndex + 1);
+                        addMove(selectIndex - 1);
+                        addMove(selectIndex + RN + 1);
+                        addMove(selectIndex + RN - 1);
+                        addMove(selectIndex - RN + 1);
+                        addMove(selectIndex - RN - 1);
+                        break;
+                }
+
+                for (int i = 0; i < moveList.Count; i++) renderSquare(chessBoardPositions[moveList[i]], Color.FromArgb(100, ColorC));
+
+            }
+
+            if (mouseHoverIndex != -1)
+            {
+                if (turn)
+                {
+                    if (chessBoard[mouseHoverIndex] > 0) renderSquare(chessBoardPositions[mouseHoverIndex], Color.FromArgb(50, ColorC));
+                }
+                else
+                {
+                    if (chessBoard[mouseHoverIndex] < 0) renderSquare(chessBoardPositions[mouseHoverIndex], Color.FromArgb(50, ColorC));
+                }
+            }
+
+            SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (mouseHoverIndex != -1)
+            {
+                if (selectIndex != -1)
+                {
+                    int moveIndex = -1;
+                    for (int i = 0; i < moveList.Count; i++) if (mouseHoverIndex == moveList[i]) moveIndex = moveList[i];
+                    if (moveIndex != -1)
+                    {
+                        movePiece(selectIndex, moveIndex);
+                        if (getY(moveIndex) == 0 || getY(moveIndex) == 7)
+                        {
+                            if (chessBoard[moveIndex] == P) chessBoard[moveIndex] = Q;
+                            else if (chessBoard[moveIndex] == -P) chessBoard[moveIndex] = -Q;
+                        }
+                        mouseHoverIndex = -1;
+                        selectIndex = -1;
+                        moveIndex = -1;
+                        swapTurn();
+                    }
+                }
+            }
+
+            if (mouseHoverIndex != -1)
+            {
+                if (chessBoard[mouseHoverIndex] != E)
+                {
+                    if (mouseHoverIndex == selectIndex) selectIndex = -1;
+                    else
+                    {
+                        if (turn)
+                        {
+                            if (chessBoard[mouseHoverIndex] > 0) selectIndex = mouseHoverIndex;
+                        }
+                        else
+                        {
+                            if (chessBoard[mouseHoverIndex] < 0) selectIndex = mouseHoverIndex;
+                        }
+                    }
+                }
+                else selectIndex = -1;
+            }
+            base.OnMouseDown(e);
+        }
+
+        void renderImage(Vector2d _pos, TysonBitmap _pic)
+        {
+            Toolbox.rIMAGE(_pic, _pos.X, _pos.Y, 25, 52);
+        }
+
+        void movePiece(int _indexA, int _indexB)
+        {
+            chessBoard[_indexB] = chessBoard[_indexA];
+            chessBoard[_indexA] = E;
+        }
+
+        int getX(int _index)
+        {
+            return _index % RN;
+        }
+
+        int getY(int _index)
+        {
+            return _index / RN;
+        }
+
+        bool isSpaceEmpty(int _index)
+        {
+            bool export = false;
+            if (isValidIndex(_index))
+            {
+                if (chessBoard[_index] == E) export = true;
+            }
+            return export;
+        }
+
+        void addBishopMoves(int _si)
+        {
+            for (int i = 0; i < RN - (_si % RN) - 1; i++)
+            {
+                int q = (_si + (RN * (i + 1))) + (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < _si % RN; i++)
+            {
+                int q = (_si + (RN * (i + 1))) - (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < RN - (_si % RN) - 1; i++)
+            {
+                int q = (_si - (RN * (i + 1))) + (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < _si % RN; i++)
+            {
+                int q = (_si - (RN * (i + 1))) - (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+        }
+
+        void addRookMoves(int _si)
+        {
+            for (int i = 0; i < RN; i++)
+            {
+                int q = _si + (RN * (i + 1));
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < RN; i++)
+            {
+                int q = _si - (RN * (i + 1));
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < RN - (_si % RN) - 1; i++)
+            {
+                int q = _si + (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+            for (int i = 0; i < _si % RN; i++)
+            {
+                int q = _si - (i + 1);
+                addMove(q);
+                if (!isSpaceEmpty(q)) break;
+            }
+        }
+
+        void addMove(int moveIndex)
+        {
+            if (isValidIndex(moveIndex))
+            {
+                if (turn)
+                {
+                    if (chessBoard[moveIndex] <= 0) moveList.Add(moveIndex);
+                }
+                else
+                {
+                    if (chessBoard[moveIndex] >= 0) moveList.Add(moveIndex);
+                }
+            }
+        }
+
+        void swapTurn()
+        {
+            turn = !turn;
+            if (turn) ColorC = Color.Red;
+            else ColorC = Color.Blue;
+        }
+
+        bool isValidIndex(int _index)
+        {
+            return (_index > -1 && _index < SQRNUM);
+        }
+
+        int getHover(Vector2d m)
+        {
+            int export = -1;
+
+            for (int i = 0; i < SQRNUM; i++)
+            {
+                Vector2d c = chessBoardPositions[i];
+                if (m.X < c.X + HALFSQRSIZE &&
+                    m.X > c.X - HALFSQRSIZE &&
+                    m.Y < c.Y + HALFSQRSIZE &&
+                    m.Y > c.Y - HALFSQRSIZE) export = i;
+            }
+
+            return export;
+        }
+
+        void renderSquare(Vector2d _pos, Color _hue)
+        {
+            Toolbox.rBOX(new Vector2d(_pos.X - HALFSQRSIZE, _pos.Y - HALFSQRSIZE), SQRSIZE, _hue);
+        }
+    }
+}
